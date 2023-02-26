92d332ef2d2d24b779a263963fdaaabc955d0e71
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2023-02-25 21:44:09 -0300

    refactor: refactoring of deterministic partition key functionality

--- dpk.js
+++ dpk.js
@@ -1,28 +1,34 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
+const createStringHash = data => {
+  data = JSON.stringify(data);
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+};
+
+// export to create unit tests
+exports.createStringHash = createStringHash;
+
+exports.deterministicPartitionKey = event => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (event === undefined || event === null) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
+  if (event.hasOwnProperty("partitionKey")) {
+    let candidate = event.partitionKey;
+
     if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
     }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+
+    if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+      return createStringHash(candidate);
+    }
+
+    return candidate;
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+
+  return createStringHash(event);
 };
